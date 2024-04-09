40efb53877bb5f7852629e42af102c569379b0d2
leandrodev01 <leandrosantos8877@gmail.com>
2024-04-09 16:47:32 -0300

    .

---
+++ 6-Props e Events/1-vue-components-comunicacao/src/components/FilmesListaIten.vue
@@ -0,0 +1,30 @@
+<template>
+    <li class="list-group-item">
+        <span>{{ filme.titulo }} | {{ filme.ano }}</span>
+        <button @click="selecionar" class="btn btn-secondary float-right">Selecionar</button>
+    </li>
+</template>
+
+<script>
+
+import { eventBus } from './../main'
+
+export default {
+    props: {
+        filme:{
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        selecionar(event) {
+            console.log(event)
+             //this.$emit('selecionarFilme', this.filme)
+            //eventBus.$emit('selecionarFilme',this.filme)
+            eventBus.selecionarFilme(this.filme)
+        }
+    },
+    
+   
+}
+</script>
