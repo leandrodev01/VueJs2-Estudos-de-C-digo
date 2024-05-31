9e33d3d872e10f3f1db14a81274cf80b7b211f9e
leandrodev01 <leandrosantos8877@gmail.com>
2024-05-31 19:02:09 -0300

    .

---
+++ 10-Vue Router/vue-rotas/src/components/contatos/ContatosListaIten.vue
@@ -0,0 +1,38 @@
+<template>
+    <li>
+        <span> {{ contato.nome}} </span>
+        <router-link 
+                :to="{ 
+                    path: `/contatos/${contato.id}`,
+                }"
+                class="btn btn-info btn-sm float-right">
+                Detalhes
+        </router-link> 
+
+        <!--<button
+            class="btn btn-info btn-sm float-right"
+            @click="verDetalhes">
+                Detalhes
+        </button> -->
+    </li>
+</template>
+<script>
+export default {
+    props: {
+        contato: {
+            type: Object,
+            required:true
+        }
+    },
+    methods: {
+        verDetalhes() {
+            // Métodos alternativos para navegação programática, caso necessário no futuro:
+            //this.$router.push('/contatos/${this.contato.id}')
+            //this.$router.push({ path: '/contatos/${this.contato.id}' })
+            //this.$router.push({ name: 'contato', params:{id: this.contato.id} })
+            //this.$router.replace({ name: 'contato', params:{id: this.contato.id} })
+        }
+    }
+}
+</script>
+
